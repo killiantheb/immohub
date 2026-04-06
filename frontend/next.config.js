29f3b8d91e5d62e8f93db3e6ce385ef54fb6916a
tikina1 <killian.thebaud@gmail.com>
2026-04-06 18:20:37 +0200

    fix: disable Sentry sourcemaps when no auth token

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,15 +15,15 @@ const nextConfig = {
 };
 
 module.exports = withSentryConfig(nextConfig, {
-  // Sentry webpack plugin options
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
   authToken: process.env.SENTRY_AUTH_TOKEN,
-
-  // Only upload source maps in CI/production
   silent: true,
   widenClientFileUpload: true,
   hideSourceMaps: true,
   disableLogger: true,
-  automaticVercelMonitors: true,
+  automaticVercelMonitors: false,
+  sourcemaps: {
+    disable: !process.env.SENTRY_AUTH_TOKEN,
+  },
 });
